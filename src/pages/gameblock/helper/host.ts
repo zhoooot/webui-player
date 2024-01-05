f01aee1e3563eb681d84c5e7960ccad90af5713e
HOANG THE ANH <therealhoangtheanh@gmail.com>
2024-01-05 23:20:16 +0700

    Continue link to Server

---
+++ src/pages/gameblock/helper/host.ts
@@ -0,0 +1,17 @@
+export const extractQuestion = (data: any) => {
+    const { partyid, question } = data;
+      const { content, time, allow_power, url, answers, correct_answer } = question;
+      let answers_list: string[] = [];
+      for (let i = 0; i < question.answers.length; i++) {
+        let answer = question.answers[i].content;
+        answers_list.push(answer);
+      }
+      return {
+        content: content,
+        answers: answers_list,
+        correctAnswer: correct_answer,
+        time: time,
+        permit: allow_power,
+      }
+}
+
--- src/pages/gameblock/host.tsx
+++ src/pages/gameblock/host.tsx
@@ -7,70 +7,49 @@ import router from "next/router";
 import { Socket } from "socket.io-client";
 import { Host } from "@/logic/host";
 import { Router } from "next/router";
+import { extractQuestion } from "./helper/host";
 
-// const quizData: string | any[] = [
-//   {
-//     question: "What is the capital of France?",
-//     answers: ["Paris", "London", "Berlin", "Madrid"],
-//     correctAnswer: 1,
-//   },
-//   {
-//     question: "What is the ?",
-//     answers: ["aaaa", "don", "bbb", "ssss"],
-//     correctAnswer: 2,
-//   },
-//   {
-//     question: "What is the capital of Germany?",
-//     answers: ["Berlin", "Paris", "London", "Madrid"],
-//     correctAnswer: 3,
-//   },
-// ];
-
-// const list: string | any[] = [
-//   { name: "a", score: 13 },
-//   { name: "b", score: 12 },
-//   { name: "c", score: 35 },
-//   { name: "d", score: 42 },
-//   { name: "e", score: 5 },
-//   { name: "f", score: 63 },
-//   { name: "g", score: 7 },
-//   { name: "h", score: 28 },
-//   { name: "i", score: 9 },
-//   { name: "j", score: 10 },
-//   { name: "k", score: 113 },
-//   { name: "l", score: 192 },
-//   { name: "m", score: 137 },
-//   { name: "n", score: 144 },
-//   { name: "o", score: 115 },
-//   { name: "p", score: 163 },
-// ];
+const list: string | any[] = [
+  { name: "a", score: 13 },
+  { name: "b", score: 12 },
+  { name: "c", score: 35 },
+  { name: "d", score: 42 },
+  { name: "e", score: 5 },
+  { name: "f", score: 63 },
+  { name: "g", score: 7 },
+  { name: "h", score: 28 },
+  { name: "i", score: 9 },
+  { name: "j", score: 10 },
+  { name: "k", score: 113 },
+  { name: "l", score: 192 },
+  { name: "m", score: 137 },
+  { name: "n", score: 144 },
+  { name: "o", score: 115 },
+  { name: "p", score: 163 },
+];
 
 const Quizzes_Host = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState({
+    content: router.query.content as string,
+    answers: router.query.answers as string[],
+    correctAnswer: parseInt(router.query.correct_answer as string),
+    time: parseInt(router.query.time as string),
+    permit: (router.query.allow_power as string) === "true",
+  });
   const [phase, setPhase] = useState(0);
   const [socket, setSocket] = useState<Socket | null>(null);
-
-  console.log("The first question is ", router.query.answers);
-
-  let questions: [
-    {
-      content: string;
-      answers: [string];
-      correctAnswer: number;
-      time: number;
-      permit: boolean;
-    }
-  ] = [{content: router.query.content as string, answers: router.query.answers as [string], correctAnswer: parseInt(router.query.correct_answer as string), time: parseInt(router.query.time as string), permit: router.query.allow_power as string === "true"}];
 
   useEffect(() => {
     const newSocket = Host.getHostClient().socketClient;
 
     newSocket.on("connect", () => {
       console.log("Connected to socket server");
-      newSocket.emit("host", {
-        room: router.query.pin as string,
-        username: "host",
-      });
+      if (typeof window !== "undefined") {
+        newSocket.emit("host", {
+          room: localStorage.getItem("hostpin") as string,
+          username: "host",
+        });
+      }
     });
 
     setSocket(newSocket);
@@ -88,27 +67,19 @@ const Quizzes_Host = () => {
       console.log("Start event received", message);
     });
     socket?.on("skipped", (message: any) => {
-      setPhase(phase + 1)
+      setPhase(phase + 1);
     });
     socket?.on("ranking", (message: any) => {
       console.log("Ranking event received", message);
     });
     socket?.on("show-question", (message: any) => {
-      console.log("Question event received", message);
-      const { partyid, msgcontent } = message;
-      const { question, time, allow_power, url, answers, correct_answer } = msgcontent;
-      questions.push({
-        content: question,
-        answers: answers,
-        correctAnswer: correct_answer,
-        time: time,
-        permit: allow_power,
-      });
+      const question = extractQuestion(message);
+      setCurrentQuestion(question);
     });
     socket?.on("result", (message: any) => {
       console.log("Result event received", message);
     });
-    socket?.on("end", (message: any) => {
+    socket?.on("final-ranking", (message: any) => {
       router.replace("/gameover");
     });
   });
@@ -122,12 +93,12 @@ const Quizzes_Host = () => {
   return (
     <div>
       {
-        <div key={currentQuestion}>
+        <div>
           {/*Phase 1*/}
           {phase === 0 && (
             <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-200">
               <div className="font-bold text-4xl text-center h-full align-middle flex items-center">
-                {questions[currentQuestion].content}
+                {currentQuestion.content}
               </div>
               <div className="self-end w-full h-auto mb-8">
                 <div className="col items-center justify-center">
@@ -148,16 +119,16 @@ const Quizzes_Host = () => {
               <Phase2
                 onComplete={() => {
 
-                  setPhase(2);
                 }}
                 next={() => {
-                  socket?.emit("question-skip", {
-                    room: router.query.pin as string,
-                  });
-                  setPhase(2);
+                  if (typeof window !== "undefined") {
+                    socket?.emit("question-skip", {
+                      room: localStorage.getItem("hostpin") as string,
+                    });
+                  }
                 }}
-                duration={questions[currentQuestion].time}
-                quizData={questions[currentQuestion]}
+                duration={currentQuestion.time}
+                quizData={currentQuestion}
               />
             </>
           )}
@@ -167,10 +138,17 @@ const Quizzes_Host = () => {
             <div>
               <Phase3
                 next={() => {
-                  setPhase(3);
+                  if (typeof window !== "undefined") {
+                    socket?.emit("ranking", {
+                      room: localStorage.getItem("hostpin") as string,
+                    });
+                    socket?.emit("show-question", {
+                      room: localStorage.getItem("hostpin") as string,
+                    });
+                  }
                 }}
-                duration={-1}
-                quizData={questions[currentQuestion]}
+                duration={5}
+                quizData={currentQuestion}
                 quizResult={[4, 3, 2, 5]}
               />
             </div>
@@ -182,7 +160,6 @@ const Quizzes_Host = () => {
               <Phase4
                 next={() => {
                   setPhase(0);
-                  setCurrentQuestion(currentQuestion + 1);
                 }}
                 list={list}
               />
