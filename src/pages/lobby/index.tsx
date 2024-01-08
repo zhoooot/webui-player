65cb6fd5e970e3fece95689e214c19392c9170e9
Phạm Võ Quỳnh Như <phamvoquynhnhu24@gmail.com>
2024-01-08 23:14:15 +0700

    update lobby

--- src/pages/lobby/index.tsx
+++ src/pages/lobby/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/router";
 import Pin from "./components/head_line_component/pin";
 import Ad from "./components/head_line_component/ad";
@@ -9,6 +9,13 @@ import Player from "./components/body_component/player";
 import { Socket, io } from "socket.io-client";
 import { Host } from "@/logic/host";
 import { ClientPlayer } from "@/logic/client-player";
+import Image from "next/image";
+import Image0 from '/public/icons/random.png';
+import Image1 from '/public/images/background/spring.png'
+import Image2 from '/public/images/background/summer.png';
+import Image3 from '/public/images/background/autumn.jpeg';
+import Image4 from '/public/images/background/winter.jpeg';
+
 
 let first_question = {
   content: "",
@@ -24,7 +31,32 @@ const Lobby = () => {
   const router = useRouter();
   const [isVisible, setIsVisible] = useState(false);
   const [socket, setSocket] = useState<Socket | null>(null);
-
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+  const [background, setBackground] = useState(0);
+  const musicTitles = ["Is It Over Now?", "The Great War", "End Game", "Born To Die"];
+  const [musicTitle, setMusicTitle] = useState(musicTitles[0]);
+  const [audioSrc, setAudioSrc] = useState("/music/is-it-over-now.mp3");
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setMusicTitle(e.target.value);
+  };
+  useEffect(() => {
+    switch (musicTitle) {
+      case "Is It Over Now?":
+        setAudioSrc("/music/is-it-over-now.mp3");
+        break;
+      case "The Great War":
+        setAudioSrc("/music/the-great-war.mp3");
+        break;
+      case "End Game":
+        setAudioSrc("/music/end-game.mp3");
+        break;
+      case "Born To Die":
+        setAudioSrc("/music/born-to-die.mp3");
+        break;
+      default:
+        setAudioSrc("/music/is-it-over-now.mp3");
+    }
+  }, [musicTitle]);
   useEffect(() => {
     setTimeout(() => {
       setIsVisible(true)
@@ -123,25 +155,102 @@ const Lobby = () => {
 
   }
   return (
-    <div className="bg-base-200 flex  flex-col h-screen justify-start items-center">
-      {/* Headline*/}
-      <div className="flex flex-row justify-between mt-4 items-center gap-2">
-        <Ad />
-        <Pin pin={pin} />
-        <div className="flex flex-col items-center justify-between h-full w-36 gap-3">
-          <div className="flex flex-row justify-between h-full w-full gap-2">
-            <PlayerCount count={players.length} />
-            <Setting onClick={() => { }} />
+    <div className="flex flex-row"
+    >
+      <audio autoPlay loop src={audioSrc} />;
+
+      <div className="flex grow flex-col h-screen justify-start items-center"
+        style={
+
+          background == 0 ? {
+            backgroundImage: `url('/images/background/spring.png')`, backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          } :
+            background == 1 ? {
+              backgroundImage: `url('/images/background/summer.png')`, backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            } :
+              background == 2 ? {
+                backgroundImage: `url('/images/background/autumn.jpeg')`, backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              } :
+                background == 3 ? {
+                  backgroundImage: `url('/images/background/winter.jpeg')`, backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                } :
+                  { backgroundImage: `url('/images/background/spring.png')` }
+
+        }>
+        {/* Headline*/}
+        <div className="flex flex-row justify-between mt-4 items-center gap-2">
+          <Ad />
+          <Pin pin={pin} />
+          <div className="flex flex-col items-center justify-between h-full w-36 gap-3">
+            <div className="flex flex-row justify-between h-full w-full gap-2">
+              <PlayerCount count={players.length} />
+              <Setting onClick={() => {
+                setSidebarVisible(!isSidebarVisible);
+
+              }} />
+            </div>
+            <Start onClick={handleStart} />
           </div>
-          <Start onClick={handleStart} />
+        </div>
+
+        {/* Body */}
+        <div className="flex flex-row p-4 justify-center items-start mt-10 w-11/12">
+          {players.map((player, index) => (
+            <Player key={player} name={player} new_member={isNew} onClick1={() => onClickRemove(index)} index={index} />
+          ))}
+
         </div>
       </div>
+      <div>
+        {isSidebarVisible && (<div className="w-96 p-5">
+          <div className="font-bold text-2xl text-left">Settings</div>
+          <div className="font-bold text-2xl mt-5 mb-2">Hosting</div>
+          <span className="flex flex-row">
+            <Image src={Image0} alt="SVG Icon" className="mr-2" width={20} height={20} />
+            <span className="text-base font-semibold grow">Randomize order of answers</span>
+            <input type="checkbox" className=" toggle toggle-success" />
+          </span>
+          <div className="font-bold text-2xl mt-5 mb-2">Theme</div>
+          <div className="grid overflow-x-scroll p-2">
+            <div className="flex flex-row gap-2 ">
+              <div className="w-40  rounded-lg bg-yellow-100 hover:ring-4 ring-purple-400"
+                onClick={() => { setBackground(0) }}>
+                <Image src={Image1} alt="spring" className="h-24 rounded-tl-lg rounded-tr-lg" />
+                <div className="p-1 text-center tracking-wide">Spring</div>
+              </div>
+              <div className="w-40  rounded-lg bg-blue-100 hover:ring-4 ring-purple-400 "
+                onClick={() => { setBackground(1) }}>
+                <Image src={Image2} alt="summer" className="h-24 rounded-tl-lg rounded-tr-lg" />
+                <div className="p-1 text-center tracking-wide">Summer</div>
+              </div>
+              <div className="w-40  rounded-lg bg-red-100 hover:ring-4 ring-purple-400"
+                onClick={() => { setBackground(2) }}>
+                <Image src={Image3} alt="autumn" className="h-24 rounded-tl-lg rounded-tr-lg" />
+                <div className="p-1 text-center tracking-wide">Autumn</div>
+              </div>
+              <div className="w-40  rounded-lg bg-gray-200 hover:ring-4 ring-purple-400"
+                onClick={() => { setBackground(3) }}>
+                <Image src={Image4} alt="winter" className="h-24 rounded-tl-lg rounded-tr-lg" />
+                <div className="p-1 text-center tracking-wide">Winter</div>
+              </div>
+            </div>
+          </div>
+          <div className="font-bold text-2xl mt-5 mb-2">Background Music</div>
 
-      {/* Body */}
-      <div className="flex flex-row p-4 justify-center items-start mt-10 w-11/12">
-        {players.map((player, index) => (
-          <Player key={player} name={player} new_member={isNew} onClick1={() => onClickRemove(index)} index={index} />
+          <select value={musicTitle} onChange={handleSelectChange} className="w-full ring-2 ring-gray-300 rounded-lg p-2 
+          hover:ring-gray-400">
+        {musicTitles.map((title, index) => (
+          <option key={index} value={title}>
+            {title}
+          </option>
         ))}
+      </select>
+        </div>
+        )}
 
       </div>
     </div>
