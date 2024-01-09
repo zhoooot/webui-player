1f87063ad1ef3fc829bd936430c6691d3d7e0d18
Phạm Võ Quỳnh Như <phamvoquynhnhu24@gmail.com>
2024-01-09 14:13:34 +0700

    push push push ei

--- src/pages/get-ready/index.tsx
+++ src/pages/get-ready/index.tsx
@@ -1,20 +1,41 @@
 // pages/index.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import Loading from './components/loading';
-import {Player} from '../../logic/player';
+import { Player } from '../../logic/player';
 import router from "next/router";
 import { ClientPlayer } from '@/logic/client-player';
+import { Socket } from 'socket.io-client';
 
 const Home: React.FC = () => {
 
-  Player.getPlayerClient().socketClient.on('start', (message: any) => {
-    router.replace('../gameblock/player')
+  const [socket, setSocket] = React.useState<Socket | null>(null);
+
+  useEffect(() => {
+    const newSocket = Player.getPlayerClient().socketClient;
+    if (newSocket) { newSocket.emit("join", {
+      room: localStorage.getItem("pin"),
+      username: localStorage.getItem("username"),
+    });}
+   
+    
+    setSocket(newSocket);
+    return () => {
+      if (socket) socket.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    socket?.on('start', (message: any) => {
+      router.replace('../gameblock/player')
+    });
   })
-  const name=localStorage.getItem("username") || "shut the fuk up";
+
+ 
+  const name = localStorage.getItem("username") || "shut the fuk up";
   return (
     <div className="mx-auto  w-screen h-screen bg-gray-100">
-      <Loading name={name} point={2222} index={1}/>
-        </div>
+      <Loading name={name} point={2222} index={1} />
+    </div>
   );
 };
 
